2d94783a77a44e07075d81399bd069ac82657758
Jonathan <JohnAnBer@gmail.com>
2026-02-08 19:47:38 -0500

    wow, this took me way too long

---
+++ app/src/fetch-helpers.js
@@ -0,0 +1,33 @@
+async function getRandomPokemon() {
+  //random number generator between 1-150
+  try {
+    const id = Math.floor(Math.random() * 150) + 1;
+    //fetching pokemon data from API and returns it in json
+    const rs = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+    if (!rs.ok) {
+      throw new Error(`Operation failed`);
+    }
+    const info = await rs.json();
+    const sprite = info.sprites["front_default"];
+    const pkName = info.species["name"];
+    const type1 = info.types["0"]["type"]["name"];
+    let types = [];
+    types.push(type1);
+    let type2;
+    if (info.types["1"]) {
+      type2 = info.types["1"]["type"]["name"];
+      types.push(type2);
+    }
+    let pokemonObj = {
+      name: pkName,
+      types: types.join(", "),
+      sprite: sprite,
+    };
+    return { data: pokemonObj, error: null };
+  } catch (error) {
+    return { data: null, error: error.message };
+  }
+}
+
+getRandomPokemon().then((result) => console.log(result));
